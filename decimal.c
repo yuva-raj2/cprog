07351b423f1ca4d038cc7ac1f7a7266c130fb8a9
yuva-raj2 <106536936+yuva-raj2@users.noreply.github.com>
2023-06-12 22:29:36 +0530

    Add files via upload

---
+++ decimal.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+ int num1=4,num2=6,num3=7;
+ int result=(num1+num2)*num3/num1;
+ printf("Your output will want to be:(%d+%d) * %d/%d",num1,num2,num3,num1);
+ printf("The result of the given expression will be:%d",result);
+ printf("hi");
+}
